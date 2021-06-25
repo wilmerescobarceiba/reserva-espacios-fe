cd945e018137318168acf3b2968c4fb3d7a303cf
Wilmer Escobar <wilmer.escobar@ceiba.com.co>
2021-06-25 00:11:42 -0500

    Se agrega configuración de karma y sonar

--- karma.conf.js
+++ karma.conf.js
@@ -11,8 +11,17 @@ module.exports = function (config) {
       require('karma-junit-reporter'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-sonarqube-unit-reporter')
     ],
+    sonarQubeUnitReporter: {
+      sonarQubeVersion: 'LATEST',
+      outputFile: 'reports/ut_report.xml',
+      overrideTestDescription: true,
+      testPaths: ['./src'],
+      testFilePattern: '.spec.ts',
+      useBrowserName: false
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
@@ -28,11 +37,11 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml','junit'],
+    reporters: ['progress', 'kjhtml','junit', 'sonarqubeUnit'],
     htmlReporter: {
       outputFile: 'reports/unit/units.html',
 
-      // Optional 
+      // Optional
       pageTitle: 'Vida - Tests Unitarios',
       subPageTitle: 'Presentacion HTML de los test unitarios',
       dir: 'reports/html/'
--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { MenuItem } from '@core/modelo/menu-item';
-import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-navbar',
@@ -10,8 +9,4 @@ import { environment } from 'src/environments/environment';
 export class NavbarComponent{
   @Input()
   items: MenuItem[];
-
-  get icon() {
-    return environment.icon;
-  }
 }
